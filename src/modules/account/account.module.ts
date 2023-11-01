8850c11f290f908704600fb5ed47ac3e0a56446c
Oluwayimika Michael Oladipupo <77396635+Excellence-arch@users.noreply.github.com>
2023-11-01 16:42:20 +0100

    Fix: auth service

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { AuthService } from '../auth/auth.service'
 import { PrismaService } from 'src/Config/prisma.service';
 
 @Module({
   controllers: [AccountController],
-  providers: [AccountService, PrismaService]
+  providers: [AccountService, PrismaService, AuthService]
 })
 export class AccountModule {}
