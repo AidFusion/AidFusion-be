c10150de6923212d93bec3afb5e1b66311f09dd0
excellence-arch <oladipupomichael9@gmail.com>
2023-11-03 11:44:44 +0100

    chore: documentation

--- src/modules/meal/meal.controller.ts
+++ src/modules/meal/meal.controller.ts
@@ -2,11 +2,30 @@ import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { MealService } from './meal.service';
 import { Meals } from '@prisma/client';
 import { ResponseDto } from 'src/common/Dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('meal')
 export class MealController {
   constructor(private mealService: MealService) {}
   @Get()
+  @ApiOperation({ summary: 'Get all meals' })
+  @ApiResponse({
+    status: 201,
+    description: 'Success',
+    type: ResponseDto<Array<Meals>>,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   async getMeals(): Promise<ResponseDto<Array<Meals>>> {
     const resp: Array<Meals> = await this.mealService.getMeals();
     return {
